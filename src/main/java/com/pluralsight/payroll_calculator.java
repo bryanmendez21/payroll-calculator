7ff26f98e4356bb1b036fe3fd26ca0c178b97c42
bryan <bryanmendez21@icloud.com>
2026-04-08 23:28:11 -0400

    added calculation logic for gross pay with and without overtime

--- src/main/java/com/pluralsight/payroll_calculator.java
+++ src/main/java/com/pluralsight/payroll_calculator.java
@@ -14,6 +14,24 @@ public class payroll_calculator {
 
         System.out.println("Hourly pay: ");
         float pay_rate = payroll.nextFloat();
+
+
+
+        // Optional
+
+        if (hours > 40){
+            float regular_pay = pay_rate * 40f;
+            float overtime_hours = hours - 40.0f;
+
+            float overtime_pay = overtime_hours *(pay_rate * 1.5f);
+            float gross_pay_with_overtime = overtime_pay + regular_pay;
+            System.out.printf("%s Gross pay with over time is: %.2f%n", full_name, gross_pay_with_overtime);
+        }
+        else {
+            float regular_pay = hours * pay_rate;
+            System.out.printf("%s Gross pay is: %.2f%n", full_name, regular_pay);
+        }
+
     }
 
 }
