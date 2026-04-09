6f7de8a55a6808edd9d48cc9313cbe07df969f7b
bryan <bryanmendez21@icloud.com>
2026-04-08 22:53:56 -0400

    Added scanner inputs for name,hours worked, and hourly pay.

--- src/main/java/com/pluralsight/payroll_calculator.java
+++ src/main/java/com/pluralsight/payroll_calculator.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 
 public class payroll_calculator {
     public static void main(String[] args) {
+        Scanner payroll = new Scanner(System.in);
 
+        System.out.println("what's your name: ");
+        String full_name = payroll.nextLine();
+
+        System.out.println("Hours worked: ");
+        float hours = payroll.nextFloat();
+
+        System.out.println("Hourly pay: ");
+        float pay_rate = payroll.nextFloat();
     }
 
 }
